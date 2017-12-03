e2fd79ff1bb158bb1db36822e910261a03c6fc06
Gangemstyle850 <carterman123456@outlook.com>
2017-12-03 00:46:32 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- api/submit.php
+++ api/submit.php
@@ -183,10 +183,10 @@
 
                     //Run Insert Query On Selected Database And Table
                         if (mysqli_query($dbc, $sql) == true){
-                            print ("Submition Was Successful!!!<br>Your Report ID Is: "); print('"'); print("<a href='../../view/indivReport.php?".$id."'>".$id."</a>"); print('"'); print("!<br>");
+                            print ("Submition Was Successful!!!<br>Your Report ID Is: "); print('"'); print("<a href='/view/indiv.php?".$id."'>".$id."</a>"); print('"'); print("!<br>");
                         }
                         else{
-                            print ("Failed To Submit!!! Please Wait And Try Again, Or Contact The Site Administrator (Info On <a href='../../../pages/contact/index.html' target='_blank'>Contact Page</a>) If The Problem Persists, And Please Include, ERROR CODE: 3<br>");
+                            print ("Failed To Submit!!! Please Wait And Try Again, Or Contact The Site Administrator (Info On <a href='/contact' target='_blank'>Contact Page</a>) If The Problem Persists, And Please Include, ERROR CODE: 3<br>");
         
                             if ($debug == true){print ("SQL Error Message: " . mysql_error() . "<br>");}
                             }
