53c63e077a377fbb72647fba47a88ee22f0b02b7
Gangemstyle850 <carterman123456@outlook.com>
2017-12-02 23:49:23 -0500

    Added Code To Prevent Repeat Submition IDs
    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- api/submit.php
+++ api/submit.php
@@ -36,15 +36,11 @@
 		
     <p>
         <?php
+		
             //Debug Options
                 $debug = "true";
-                $dbSend = "true";
-
-            //Debug Error Codes: 
-                //1 - Uploaded File Is Not A Video
-            //Display Test Message
-                print("Yes, I Do Work!!!<br>");
-
+                $dbSend = "true";				
+				
             //Form Imports
                 $reprt = $_POST["Reprt"];
                 $plateID = $_POST["plateID"];
@@ -59,10 +55,45 @@
                 $incLat = $_POST["incLat"];
                 $incLon = $_POST["incLon"];
 
-            //Generate Unique 7 Digit Id
-                $id = rand(1111111, 9999999);
             //Combine Input Into Useable Date
                 $incDate = $incYear . '/' . $incMonth . '/' . $incDay;
+		
+		
+		
+			//DB Vars
+                $dbHost = "127.0.0.1";
+                $dbUser = "root";
+                $dbPass = "password!";
+                $db = "reportdb";
+                $table = "reports";
+                $sql = "INSERT INTO $table (id, reprt, plateID, plateState, incDate, usrName, perpName, usrMail, perpMail, incLat, incLon, uploadPath) VALUES ('$id', '$reprt', '$plateID', '$plateState', '$incDate', '$usrName', '$perpName', '$usrMail', '$perpMail', '$incLat', '$incLon', '$uploadPath');";
+			
+			//Connect To Database Server
+                        $dbc = mysqli_connect($dbHost, $dbUser, $dbPass)
+                            or die("Unable To Connect To Database, Please Wait And Try Again, Or Contact The Site Administrator (Info On Contact Page) If The Problem Persists, And Please Include, ERROR CODE: 1<br>");
+                            if ($debug == true){print ("Connected To Database Server!!!<br>");}
+		
+			
+			//Generate 7 Digit Id, And Make Shure It Is Unique	
+			while($valid == 0){
+				$id = rand(1111111, 9999999);
+				
+				$sqlIdCheck = "SELECT * FROM  {$table} WHERE id={$id}";
+				
+				$idCheckResult = mysqli_query($sqlIdCheck);
+				
+				while($idRow=mysql_fetch_array($idCheckResult)){
+					$existId = $idRow['id'];
+					if($existId == $id){
+						$valid = 0;
+						break;
+					}else if($existId !== $id){
+						$valid = 1;
+						break(2);
+					}
+				}
+			}
+			
 			
             
             //UPLOAD THE FILEZ!!!
@@ -141,11 +172,6 @@
             //Send To DB (If Aplicable)
                 if($dbSend == true){
                 //Database Mumbo-Jumbo (The Important Stuffs)
-                    //Connect To Database Server
-                        $dbc = mysqli_connect($dbHost, $dbUser, $dbPass)
-                            or die("Unable To Connect To Database, Please Wait And Try Again, Or Contact The Site Administrator (Info On Contact Page) If The Problem Persists, And Please Include, ERROR CODE: 1<br>");
-                            if ($debug == true){print ("Connected To Database Server!!!<br>");}
-
                     //Select Database On Server
                         $SelectedDB = mysqli_select_db($dbc, $db)
                             or die("Could Not Find And Select Database On Server :'( Please Wait And Try Again, Or Contact The Site Administrator (Info On Contact Page) If The Problem Persists, And Please Include, ERROR CODE: 2<br>");
