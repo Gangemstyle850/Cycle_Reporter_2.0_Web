5f3a778be268438ad2462d7e31d1219ead659532
Gangemstyle850 <carterman123456@outlook.com>
2017-12-07 17:57:03 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- api/mobile.php
+++ api/mobile.php
@@ -90,6 +90,8 @@
                             consoleWrite ("Failed To Submit!!! Please Wait And Try Again, Or Contact The Site Administrator (Info On <a href='../../../pages/contact/index.html' target='_blank'>Contact Page</a>) If The Problem Persists, And Please Include, ERROR CODE: 3");
         
                             if ($debug == true){consoleWrite ("SQL Error Message: " . mysqli_error ($dbc) . "");}
+							$json_status = null;
+							$json_result = null;
                             }
                         }
                     //Close Connection
