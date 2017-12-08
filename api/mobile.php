ce5d0a9ca0a250df099fe11f98ad4b6e7b81eef9
Gangemstyle850 <carterman123456@outlook.com>
2017-12-08 00:34:33 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- api/mobile.php
+++ api/mobile.php
@@ -80,14 +80,14 @@
                         if($debug){consoleWrite("Attempting SQL Query...");}
                         if (mysqli_query($dbc, $sql) == true){
                             consoleWrite ("Submition Was Successful!!!Your Report ID Is: "); consoleWrite('"'); consoleWrite("<a href='../../view/indivReport.php?".$id."'>".$id."</a>"); consoleWrite('"'); consoleWrite("!");
-							$json_status = "false";
+							$json_status = "true";
 							$json_result = $id;
 						}
                         else{
                             consoleWrite ("Failed To Submit!!! Please Wait And Try Again, Or Contact The Site Administrator (Info On <a href='../../../pages/contact/index.html' target='_blank'>Contact Page</a>) If The Problem Persists, And Please Include, ERROR CODE: 3");
         
                             if ($debug == true){consoleWrite ("SQL Error Message: " . mysqli_error ($dbc) . "");}
-							$json_status = null;
+							$json_status = "false";
 							$json_result = null;
                             }
                         }
