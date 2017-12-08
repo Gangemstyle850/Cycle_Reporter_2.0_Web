54362e72cc8bf6ee3007817af6dda32ed7f5c26b
Gangemstyle850 <carterman123456@outlook.com>
2017-12-08 00:24:53 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- signup/index.php
+++ signup/index.php
@@ -7,40 +7,7 @@
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>  
 <body>  
-<div id="main">
-<?php
-if(!empty($_POST['username']) && !empty($_POST['password']))
-{
-    $username = mysql_real_escape_string($_POST['username']);
-    $password = md5(mysql_real_escape_string($_POST['password']));
-    $email = mysql_real_escape_string($_POST['email']);
-     
-     $checkusername = mysql_query("SELECT * FROM users WHERE Username = '".$username."'");
-      
-     if(mysql_num_rows($checkusername) == 1)
-     {
-        echo "<h1>Error</h1>";
-        echo "<p>Sorry, that username is taken. Please go back and try again.</p>";
-     }
-     else
-     {
-        $registerquery = mysql_query("INSERT INTO users (Username, Password, EmailAddress) VALUES('".$username."', '".$password."', '".$email."')");
-        if($registerquery)
-        {
-            echo "<h1>Success</h1>";
-            echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
-        }
-        else
-        {
-            echo "<h1>Error</h1>";
-            echo "<p>Sorry, your registration failed. Please go back and try again.</p>";    
-        }       
-     }
-}
-else
-{
-    ?>
-     
+<div id="main">     
    <h1>Register</h1>
      
    <p>Please enter your details below to register.</p>
@@ -53,11 +20,6 @@ else
         <input type="submit" name="register" id="register" value="Register" />
     </fieldset>
     </form>
-     
-    <?php
-}
-?>
- 
 </div>
 </body>
 </html>
