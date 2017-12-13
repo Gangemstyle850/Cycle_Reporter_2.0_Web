4344ee130ee88ba3414aa888977f14173c3f56cf
Gangemstyle850 <carterman123456@outlook.com>
2017-12-13 12:00:37 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- api/mobile.php
+++ api/mobile.php
@@ -92,7 +92,8 @@
                         mysqli_close($dbc);
 						
 					//Write Out Result As JSON
-					print(json_encode ($json_status));</br>
+					print(json_encode ($json_status));
+					print("</br>");
 					print(json_encode ($json_result));
         ?>
     </p>
